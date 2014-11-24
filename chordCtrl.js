cfeeb0ca93ed30829c060e0ad6f96663f06fea6e
Benji Kay <okaybenji@gmail.com>
2014-11-23 22:35:40 -0600

    add chord buttons, toggle buttons, key selector

--- chordCtrl.js
+++ chordCtrl.js
@@ -2,38 +2,161 @@ var chordOrganApp = angular.module('chordOrganApp', []);
 
 chordOrganApp.controller('chordCtrl', ['$scope',
     
-    function tileCtrl($scope) {
-
-//        var Tile = function(color) {
-//            this.color = color || 'none';
-//        };
+    function chordCtrl($scope) {
         
-//        Tile.prototype = {
-//            
-//            flip: function flip() {
-//                //if tile is flipped one way, flip it back the other way
-//                this.transform = (this.transform == 'rotateY(180deg)') ? 'rotateY(0deg)' : 'rotateY(180deg)';
-//            }
-//            
-//        };
+        //arrays
+        var voices = [];
+        var defaultChord = [];
+        
+        //toggles
+        $scope.invertMode = false;
+        $scope.invertChord = false;
+        $scope.specialChord = false;
+        
+        //C major
+        defaultChord[0] = 16; //C1
+        defaultChord[1] = 28; //C2
+        defaultChord[2] = 40; //C3
+        defaultChord[3] = 44; //E3
+        defaultChord[4] = 47; //G3
+        
+        $scope.keys = [
+            { label: 'G', value: -5 },
+            { label: 'G#', value: -4 },
+            { label: 'A', value: -3 },
+            { label: 'A#', value: -2 },
+            { label: 'B', value: -1 },
+            { label: 'C', value: 0 },
+            { label: 'C#', value: 1 },
+            { label: 'D', value: 2 },
+            { label: 'D#', value: 3 },
+            { label: 'E', value: 4 },
+            { label: 'F', value: 5 },
+            { label: 'F#', value: 6 }
+        ];
+        
+        $scope.keyShift = $scope.keys[5]; //default to key of C
         
         //get the frequency in hertz of a given piano key
         function getFreq(key) {
             return Math.pow(2, (key-49)/12) * 440;
         }
         
-        $scope.start = function start() {
-            startTone(getFreq(52));
-            startTone(getFreq(56));
-            startTone(getFreq(59));
+        function minor(chord) {
+            //lower third
+            chord[3] = chord[3] - 1;
         }
         
+        function augment(chord) {
+            //raise fifth
+            chord[4] = chord[4] + 1;
+        }
+        
+        function diminish(chord) {
+            //lower third and fifth
+            chord[3] = chord[3] - 1;
+            chord[4] = chord[4] - 1;
+        }
+        
+        function invert(chord) {
+            //shift all notes to first inversion
+            chord[2] = chord[3];
+            chord[3] = chord[4];
+            chord[4] = chord[1] + 24;
+            chord[0] = chord[2] - 24;
+            chord[1] = chord[2] - 12;
+        }
+        
+        //shift all notes up (or down)
+        function shift(chord, halfSteps) {
+            for (var i=0, ii=chord.length; i<ii; i++) {
+                chord[i]+=halfSteps;
+            }
+        }
+        
+        //determine chord to play and start playing it
+        $scope.start = function start(chordNumber) {
+            
+            var chord = defaultChord.slice(); //build from default (C major) chord (slice copies by val rather than ref)
+            var invertMode = $scope.invertMode;
+            var invertChord = $scope.invertChord;
+            var specialChord = $scope.specialChord;
+            
+            switch(chordNumber) {
+                case 1:
+                    if (specialChord) {
+                        augment(chord);
+                    } else if (invertMode) {
+                        minor(chord);
+                    }
+                    break;
+                case 2:
+                    if (specialChord) {
+                        diminish(chord);
+                    } else if (!invertMode) {
+                        minor(chord);
+                    }
+                    shift(chord, 2);
+                    break;
+                case 3:
+                    if (specialChord) {
+                        diminish(chord);
+                    } else if (!invertMode) {
+                        minor(chord);
+                    }
+                    shift(chord, 4);
+                    break;
+                case 4:
+                    if (specialChord) {
+                        diminish(chord);
+                        shift(chord, 1);
+                    } else if (invertMode) {
+                        minor(chord);
+                    }
+                    shift(chord, 5);
+                    break;
+                case 5:
+                    if (specialChord) {
+                        diminish(chord);
+                        shift(chord, 1);
+                    } else if (invertMode) {
+                        minor(chord);
+                    }
+                    shift(chord, 7);
+                    break;
+                case 6:
+                    if (specialChord) {
+                        shift(chord, 1);
+                    } else if (!invertMode) {
+                        minor(chord);
+                    }
+                    shift(chord, 9);
+                    break;
+            }
+            
+            if (invertChord) {
+                invert(chord);
+            }
+            
+            //trigger one note per oscillator
+            for (var i=0, ii=chord.length; i<ii; i++) {
+                var key = chord[i] + $scope.keyShift.value;
+                voices[i].start(getFreq(key));
+            }
+        }
+        
+        //stop all oscillators
         $scope.stop = function stop() {
-            stopTone();
+            for (var i=0, ii=defaultChord.length; i<ii; i++) {
+                voices[i].stop();
+            }
         }
 
+        //initialize one monosynth per voice
         var init = function init() {
-            //do initial stuff
+            for (var i=0, ii=defaultChord.length; i<ii; i++) {
+                voices[i] = new monosynth;
+            }
         }
         
         init();

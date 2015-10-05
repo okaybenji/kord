a79cc690fb7ab699de30199e0f65eaa750aa5936
okaybenji <okaybenji@gmail.com>
2015-10-04 19:25:13 -0500

    update subpoly

--- subpoly.js
+++ subpoly.js
@@ -5,15 +5,14 @@
  */
 
 var Polysynth = function Polysynth(audioCtx, config) {
-
+  var synth;
   var Synth = function Synth() {
-    config = config || {};
-    config.cutoff = config.cutoff || {};
-    
-    var synth = this;
-
+    synth = this;
     synth.audioCtx = audioCtx;
     synth.voices = [];
+    
+    config = config || {};
+    config.cutoff = config.cutoff || {};
 
     for (var i = 0, ii = config.numVoices || 16; i < ii; i++) {
       synth.voices.push(new Monosynth(audioCtx, config));
@@ -27,24 +26,23 @@ var Polysynth = function Polysynth(audioCtx, config) {
 
   // apply attack, decay, sustain envelope
   Synth.prototype.start = function startSynth() {
-    this.voices.forEach(function(voice) {
+    synth.voices.forEach(function startVoice(voice) {
       voice.start();
     });
   };
 
   // apply release envelope
   Synth.prototype.stop = function stopSynth() {
-    this.voices.forEach(function(voice) {
+    synth.voices.forEach(function stopVoice(voice) {
       voice.stop();
     });
   };
 
   // get/set synth stereo width
   Synth.prototype.width = function width(newWidth) {
-    var synth = this;
-    if (synth.voices && newWidth) {
+    if (synth.voices.length > 1 && newWidth) {
       synth.stereoWidth = newWidth;
-      synth.voices.forEach(function(voice, i) {
+      synth.voices.forEach(function panVoice(voice, i) {
         var spread = 1/(synth.voices.length - 1);
         var xPos = spread * i * synth.stereoWidth;
         var zPos = 1 - Math.abs(xPos);
@@ -60,35 +58,35 @@ var Polysynth = function Polysynth(audioCtx, config) {
     var monosynthProperties = ['maxGain', 'attack', 'decay', 'sustain', 'release'];
     var monosynthCutoffProperties = ['maxFrequency', 'attack', 'decay', 'sustain'];
 
-    monosynthProperties.forEach(function(property) {
-      Synth.prototype[property] = function(newValue) {
-        synth.voices.forEach(function(voice) {
+    monosynthProperties.forEach(function createSetter(property) {
+      Synth.prototype[property] = function setValues(newValue) {
+        synth.voices.forEach(function setValue(voice) {
           voice[property] = newValue;
         });
       };
     });
 
     Synth.prototype.cutoff = {};
-    monosynthCutoffProperties.forEach(function(property) {
-      Synth.prototype.cutoff[property] = function(newValue) {
-       synth.voices.forEach(function(voice) {
-         voice.cutoff[property] = newValue;
-       });
+    monosynthCutoffProperties.forEach(function createSetter(property) {
+      Synth.prototype.cutoff[property] = function setValues(newValue) {
+        synth.voices.forEach(function setValue(voice) {
+          voice.cutoff[property] = newValue;
+        });
       };
     });
 
     Synth.prototype.waveform = function waveform(newWaveform) {
-      synth.voices.forEach(function(voice) {
+      synth.voices.forEach(function waveform(voice) {
         voice.waveform(newWaveform);
       });
     };
 
     Synth.prototype.pitch = function pitch(newPitch) {
-      synth.voices.forEach(function(voice) {
+      synth.voices.forEach(function pitch(voice) {
         voice.pitch(newPitch);
       });
     };
   })();
-  
+
   return new Synth;
 };

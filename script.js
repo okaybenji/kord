70d4896dfb40126c16b0624dfe698ed4205e9b3e
Benji Kay <okaybenji@gmail.com>
2015-11-13 17:01:19 -0600

    troubleshooting

--- script.js
+++ script.js
@@ -514,7 +514,9 @@ var panic = function panic() {
       var startChord = function startChord(e) {
         e.preventDefault();
         polysynth.lfo.depth(0); // reset lfo depth
-        refreshForce = setTimeout(updateLfo.bind(e), forceUpdateInterval);
+        refreshForce = setTimeout(function() {
+          updateLfo.bind(e)
+        }, forceUpdateInterval);
         start(chord.number);
       };
 
@@ -539,7 +541,9 @@ var panic = function panic() {
       // set lfo depth based on touch pressure
       var updateLfo = function updateLfo(e) {
         e.preventDefault();
-        refreshForce = setTimeout(updateLfo.bind(e), forceUpdateInterval);
+        refreshForce = setTimeout(function() {
+          updateLfo.bind(e)
+        }, forceUpdateInterval);
         var touches = e.touches || e.originalEvent.touches || e.originalEvent.changedTouches;
         if (!touches) {
           return;
